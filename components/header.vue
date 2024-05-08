e5a49df7bb501908c2e60a997b139b2cbb720510
GervinFung <gervinfungdaxuen@gmail.com>
2024-05-08 21:45:47 +0800

    fix(link): added link to header and footer logo

--- components/header.vue
+++ components/header.vue
@@ -3,12 +3,15 @@
 		class="flex justify-content-center mb-6 border-bottom-1 border-gray-200"
 	>
 		<div class="py-2" style="width: 80%">
-			<img
-				class="border-round-lg"
-				src="~/assets/world.png"
-				alt="Logo icon"
-				style="width: 32px; height: 32px"
-			/>
+			<NuxtLink to="/">
+				<img
+					class="border-round-lg"
+					src="~/assets/world.png"
+					alt="Logo icon"
+					style="width: 32px; height: 32px"
+				/>
+			</NuxtLink>
 		</div>
 	</div>
 </template>
+<script setup lang="ts"></script>
