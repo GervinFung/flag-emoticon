0e605a88dfc0e50392c1f2336e403344645651dd
GervinFung <gervinfungdaxuen@gmail.com>
2024-05-09 13:57:18 +0800

    format(composables): formatted files in `composables`

--- composables/use-search.ts
+++ composables/use-search.ts
@@ -1,10 +1,9 @@
-
-const useSearch=()=>{
-    const route=useRoute()
+const useSearch = () => {
+	const route = useRoute();
 
 	const { search } = route.query;
 
 	return ref(typeof search !== 'string' ? '' : search);
-}
+};
 
-export default useSearch
+export default useSearch;
